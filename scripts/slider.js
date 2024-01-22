c2adeb48e58090978de8eb8d04dc0323704543b0
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2024-01-22 11:34:12 +0700

    TASK-07: pause slider when hovering in index page

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,6 +1,12 @@
 $(".constant-simple-slider").slick({
   cssEase: "linear",
   speed: 8000,
-  autoPlay: false,
-  pauseOnHover: false,
+  pauseOnHover: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 0,
+  responsive: [{ breakpoint: 769, settings: { slidesToShow: 2 } }],
 });
